34da04a32c390ad1c67f1a36069878575dc047b3
Evan Boehs <evan@boehs.org>
2023-12-20 01:46:42 -0500

    goof

--- script.js
+++ script.js
@@ -2,7 +2,31 @@
 const el = document.getElementById("video");
 const div = document.querySelector("div");
 let source = el.querySelector("source")
-let flicks = [["maw.webm"], ["OIIAOIIA.webm", 160, 6.65], ["PHONK2.webm", 130, 2.1], ["skate.mp4", 150, 6.5], ["yum.webm"], ["mwaa.webm", 5, 5.8]]
+let flicks = [["maw.webm"], ["OIIAOIIA.webm", 160, 6.65], ["PHONK2.webm", 130, 2.1], ["goof.webm", 1000, 2], ["skate.mp4", 150, 6.5], ["yum.webm"], ["mwaa.webm", 5, 5.8]]
+
+const audioContext = new AudioContext()
+const bufferSize = 4096;
+const pinkNoise = (function () {
+    var b0, b1, b2, b3, b4, b5, b6;
+    b0 = b1 = b2 = b3 = b4 = b5 = b6 = 0.0;
+    var node = audioContext.createScriptProcessor(bufferSize, 1, 1);
+    node.onaudioprocess = function (e) {
+        var output = e.outputBuffer.getChannelData(0);
+        for (var i = 0; i < bufferSize; i++) {
+            var white = Math.random() * 2 - 1;
+            b0 = 0.99886 * b0 + white * 0.0555179;
+            b1 = 0.99332 * b1 + white * 0.0750759;
+            b2 = 0.96900 * b2 + white * 0.1538520;
+            b3 = 0.86650 * b3 + white * 0.3104856;
+            b4 = 0.55000 * b4 + white * 0.5329522;
+            b5 = -0.7616 * b5 - white * 0.0168980;
+            output[i] = b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362;
+            output[i] *= 0.11; // (roughly) compensate for gain
+            b6 = white * 0.115926;
+        }
+    }
+    return node;
+})();
 
 let burntFlicks = []
 
@@ -28,7 +52,9 @@ const funni = async () => {
         el.pause()
         el.load()
         div.style.animation = undefined
+        pinkNoise.connect(audioContext.destination)
         setTimeout(() => {
+            pinkNoise.disconnect()
             el.style.display = "initial"
             el.play()
             if (flick[1] != undefined) {
@@ -79,6 +105,7 @@ document.addEventListener("mouseleave", (e) => {
 
 div.addEventListener("click", () => {
     div.style.cursor = 'alias'
+    audioContext.resume()
     if (el.muted == false) {
         funni()
     }
